201a0eb51ef41272bd4b7f16fce94dc6cb68a0c7
unknown <ersnoblemedia@gmail.com>
2026-06-15 01:30:35 +0100

    Updated Favicon

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig(() => {
         manifest: {
           name: 'Pistis Nexus',
           short_name: 'Nexus',
-          description: 'A global admin dashboard.',
+          description: 'The Pistis Place Administrative App',
           theme_color: '#0B0118',
           background_color: '#0B0118',
           display: 'standalone',
